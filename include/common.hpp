ae07c71c2a506e390d67bff6c479b1ec82eab1ef
Little Flower <zhangshiqiao171@outlook.com>
2019-04-10 07:45:19 -0400

    Create common.hpp
    Adds initial version of common.hpp. Includes documentation for
    namespaces LF_lib and multi.

---
+++ include/common.hpp
@@ -0,0 +1,23 @@
+/**
+ * @file common.hpp
+ * Provides common content that is to be included in every header.
+ */
+
+#ifndef INC_LF_MULTI_COMMON_HPP_S71HXKSE8C
+#define INC_LF_MULTI_COMMON_HPP_S71HXKSE8C
+
+/**
+ * Little Flower's library.
+ */
+namespace LF_lib {
+
+  /**
+   * The multi library.
+   */
+  namespace multi {
+
+  }
+
+}
+
+#endif
